ece06eb65dd55815ea651e2436a956d181df9e81
Marcos Roriz <marcosrorizinf@gmail.com>
2023-11-14 12:54:12 -0300

    Fix no parse da data do excel (importar planilha)

--- src/renderer/modules/aluno/aluno-importar-ctrl.js
+++ src/renderer/modules/aluno/aluno-importar-ctrl.js
@@ -100,8 +100,10 @@ function preprocess(arquivo) {
     }
 }
 
-function ExcelDateToJSDate(date) {
-    return new Date(Math.round((date - 25569) * 86400 * 1000));
+function ExcelDateToJSDate(serial) {
+    var hours = Math.floor((serial % 1) * 24);
+    var minutes = Math.floor(((serial % 1) * 24 - hours) * 60);
+    return new Date(Date.UTC(0, 0, serial, hours - 17, minutes));
 }
 
 var dicionarioSexo = [
